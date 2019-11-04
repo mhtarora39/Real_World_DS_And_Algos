2bffee563c549cd1779ef440fefd00af75a6aea8
mhtarora39 <mhtarora39@gmail.com>
2019-11-04 14:41:35 +0530

    Soctt Bhiya

---
+++ Scott Mayers ModrenCPP/Chapter_1_type_deduction.cxx
@@ -0,0 +1,37 @@
+/*
+ * This file containt use for understanding c plus plus
+ * 
+ */
+
+
+
+#include<iostream>
+#include<vector>
+#include <typeinfo>
+
+
+//Decltype 
+
+
+//Type Deduction 
+/*
+the specification for std::type_info::name mandates that the type be treated 
+as if it had been passed to a template function as a by-value parameter.
+*/
+
+
+template<typename T>
+void f(const T& ele) {
+    std::cout << "Type : " << typeid(T).name() << std::endl;
+    std::cout << "Type ele : " << typeid(ele).name() << std::endl;
+}
+
+
+int main() {
+  auto ele = std::vector<int>(2); 
+  f(&ele);
+  return 0;
+}
+
+
+
